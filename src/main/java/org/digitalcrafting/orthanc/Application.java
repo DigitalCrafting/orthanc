08deffda1a3d87fa198a7bef68e83bad2605951e
DigitalCrafting <k.kasprzyk2@gmail.com>
2021-05-17 19:00:41 +0200

    Decoding request params

--- src/main/java/org/digitalcrafting/orthanc/Application.java
+++ src/main/java/org/digitalcrafting/orthanc/Application.java
@@ -1,25 +1,70 @@
 package org.digitalcrafting.orthanc;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import static java.util.stream.Collectors.*;
 
 public class Application {
     public static void main(String[] args) throws IOException {
         int port = 8080;
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         server.createContext("/api/hello", (exchange) -> {
-            String resp = "Hello!";
+            if (HttpMethod.GET.equals(exchange.getRequestMethod())) {
+                Map<String, List<String>> params = parseRequestParams(exchange.getRequestURI().getRawQuery());
+                String noNameText = "Anonymous";
+                String name = params.getOrDefault("name", List.of(noNameText)).stream().findFirst().orElse(noNameText);
+                String respText = String.format("Hello %s!", name);
+
+                exchange.sendResponseHeaders(200, respText.getBytes(StandardCharsets.UTF_8).length);
+                OutputStream os = exchange.getResponseBody();
+                os.write(respText.getBytes(StandardCharsets.UTF_8));
+                os.flush();
+                exchange.close();
+            } else {
+                exchange.sendResponseHeaders(405, -1);
+            }
+        });
+        server.createContext("/api/test", (exchange -> {
+            String resp = "Test!";
             exchange.sendResponseHeaders(200, resp.getBytes(StandardCharsets.UTF_8).length);
             OutputStream os = exchange.getResponseBody();
             os.write(resp.getBytes(StandardCharsets.UTF_8));
             os.flush();
             exchange.close();
-        });
+        }));
         server.setExecutor(null);
         server.start();
+    }
+
+    private static Map<String, List<String>> parseRequestParams(String query) {
+        if (query == null || query.length() == 0) {
+            return Collections.emptyMap();
+        }
+
+        return Pattern.compile("&").splitAsStream(query)
+                .map(s -> Arrays.copyOf(s.split("="), 2))
+                .collect(groupingBy(s -> decode(s[0]), mapping(s-> decode(s[1]), toList())));
+    }
+
+    private static String decode(final String encoded) {
+        try {
+            return encoded == null ? null : URLDecoder.decode(encoded, "UTF-8");
+        } catch (final UnsupportedEncodingException e) {
+            throw new RuntimeException("UTF-8 is a required encoding", e);
+        }
     }
 }
---
+++ src/main/java/org/digitalcrafting/orthanc/HttpMethod.java
@@ -0,0 +1,13 @@
+package org.digitalcrafting.orthanc;
+
+public final class HttpMethod {
+    public static final String GET = "GET";
+    public static final String PUT = "PUT";
+    public static final String POST = "POST";
+    public static final String PATCH = "PATCH";
+    public static final String DELETE = "DELETE";
+    public static final String OPTIONS = "OPTIONS";
+
+    private HttpMethod() {
+    }
+}
